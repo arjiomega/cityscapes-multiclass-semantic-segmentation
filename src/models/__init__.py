0bd5a7feb39665a81a15e3ea3973766d1422543d
Richard Joseph Omega <richardjoseph.omega@gmail.com>
2025-01-02 22:46:16 +0800

    model loader from smp

---
+++ src/models/__init__.py
@@ -0,0 +1,60 @@
+import segmentation_models_pytorch as smp
+
+architectures = {
+    'Unet': smp.Unet,
+    'UnetPlusPlus': smp.UnetPlusPlus,
+    'MAnet': smp.MAnet,
+    'Linknet': smp.Linknet,
+    'FPN': smp.FPN,
+    'PSPNet': smp.PSPNet,
+    'PAN': smp.PAN,
+    'DeepLabV3': smp.DeepLabV3,
+    'DeepLabV3Plus': smp.DeepLabV3Plus,
+}
+
+encoders = {
+    "efficientnet": {
+        "efficientnet-b0": {"weights": ["imagenet"]},
+        "efficientnet-b1": {"weights": ["imagenet"]},
+        "efficientnet-b2": {"weights": ["imagenet"]},
+        "efficientnet-b3": {"weights": ["imagenet"]},
+        "efficientnet-b4": {"weights": ["imagenet"]},
+        "efficientnet-b5": {"weights": ["imagenet"]},
+        "efficientnet-b6": {"weights": ["imagenet"]},
+        "efficientnet-b7": {"weights": ["imagenet"]},
+        "timm-efficientnet-b0": {"weights": ["imagenet", "advprop", "noisy-student"]},
+        "timm-efficientnet-b1": {"weights": ["imagenet", "advprop", "noisy-student"]},
+        "timm-efficientnet-b2": {"weights": ["imagenet", "advprop", "noisy-student"]},
+        "timm-efficientnet-b3": {"weights": ["imagenet", "advprop", "noisy-student"]},
+        "timm-efficientnet-b4": {"weights": ["imagenet", "advprop", "noisy-student"]},
+        "timm-efficientnet-b5": {"weights": ["imagenet", "advprop", "noisy-student"]},
+        "timm-efficientnet-b6": {"weights": ["imagenet", "advprop", "noisy-student"]},
+        "timm-efficientnet-b7": {"weights": ["imagenet", "advprop", "noisy-student"]},
+        "timm-efficientnet-b8": {"weights": ["imagenet", "advprop"]},
+        "timm-efficientnet-l2": {"weights": ["noisy-student"]},
+        "timm-efficientnet-lite0": {"weights": ["imagenet"]},
+        "timm-efficientnet-lite1": {"weights": ["imagenet"]},
+        "timm-efficientnet-lite2": {"weights": ["imagenet"]},
+        "timm-efficientnet-lite3": {"weights": ["imagenet"]},
+        "timm-efficientnet-lite4": {"weights": ["imagenet"]}
+    },
+    "mobilenet": {
+        "mobilenet_v2": {"weights": ["imagenet"]},
+        "timm-mobilenetv3_large_075": {"weights": ["imagenet"]},
+        "timm-mobilenetv3_large_100": {"weights": ["imagenet"]},
+        "timm-mobilenetv3_large_minimal_100": {"weights": ["imagenet"]},
+        "timm-mobilenetv3_small_075": {"weights": ["imagenet"]},
+        "timm-mobilenetv3_small_100": {"weights": ["imagenet"]},
+        "timm-mobilenetv3_small_minimal_100": {"weights": ["imagenet"]}
+    },
+    "vgg": {
+        "vgg11": {"weights": ["imagenet"]},
+        "vgg11_bn": {"weights": ["imagenet"]},
+        "vgg13": {"weights": ["imagenet"]},
+        "vgg13_bn": {"weights": ["imagenet"]},
+        "vgg16": {"weights": ["imagenet"]},
+        "vgg16_bn": {"weights": ["imagenet"]},
+        "vgg19": {"weights": ["imagenet"]},
+        "vgg19_bn": {"weights": ["imagenet"]}
+    }
+}
---
+++ src/models/model_loader.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+from config import config
+from src.models import architectures, encoders
+from src.utils.config_loader import ConfigLoader
+
+class ModelLoader:
+    def __init__(self, model):
+        self.model = model
+        self.is_from_segmentation_models = False
+
+    def load_model(self):
+        return self.model
+
+    @classmethod
+    def from_segmentation_models(
+        cls, 
+        architecture_name: str, 
+        encoder_family: str, 
+        encoder_name: str, 
+        weights_name: str, 
+        n_classes:int,
+        in_channels: int=3,
+        ):
+        if architecture_name not in architectures.keys():
+            raise ValueError(f"Architecture {architecture_name} is not available.")
+        if encoder_family not in encoders.keys():
+            raise ValueError(f"Encoder Family {encoder_family} is not available.")
+        if encoder_name not in encoders[encoder_family].keys():
+            raise ValueError(f"Encoder {encoder_name} is not available in the encoder family {encoder_family}.")
+        if weights_name not in encoders[encoder_family][encoder_name]['weights']:
+            raise ValueError(f"Weights {weights_name} are not available for encoder {encoder_name} in the encoder family {encoder_family}.")
+
+        architecture = architectures[architecture_name]
+        model = architecture(encoder_name=encoder_name, encoder_weights=weights_name, in_channels=in_channels, classes=n_classes)
+        cls.is_from_segmentation_models = True
+        return cls(model)
+    
+    @classmethod
+    def from_config(cls, config_path=Path(config.CONFIG_DIR, "train_args.json"), config: ConfigLoader = None):
+      
+        config = config if config else ConfigLoader(config_path)
+
+        return cls.from_segmentation_models(
+            architecture_name=config.architecture,
+            encoder_family=config.encoder_family,
+            encoder_name=config.encoder_name,
+            weights_name=config.weights_name,
+            n_classes=len(config.class_map)
+        )
+
+if __name__ == "__main__":
+    model_loader = ModelLoader.from_segmentation_models("DeepLabV3Plus", "efficientnet", "efficientnet-b3", "imagenet", 5)
+    model = model_loader.load_model()
+    print(model)
